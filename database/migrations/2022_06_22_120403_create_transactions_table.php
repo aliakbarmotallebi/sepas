5f0ca47e8c7fc82304d349631c4ff65ce76430c8
AliAkbar Motallebi <motallebi.aliakbar@gmail.com>
2022-06-27 16:09:28 +0430

    change name col uuid to  transaction_code

--- database/migrations/2022_06_22_120403_create_transactions_table.php
+++ database/migrations/2022_06_22_120403_create_transactions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration {
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->uuid('transaction_no');
+            $table->uuid('transaction_code');
 
             $table->string('amount')
                 ->default(0);
